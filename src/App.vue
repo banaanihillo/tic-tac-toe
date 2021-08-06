5d82b7b6ee48acba2d52b89674cb412cc39f17ed
banaanihillo <banaanihillo@protonmail.ch>
2021-08-06 13:06:03 +0300

    Added a 3-by-3 grid of buttons
    Added buttons with empty content by default,
    with X added on click,
    as well as a selected class,
    with the button not being disabled on click yet,
    nor does it support O yet

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span
+      v-for="tile in tiles"
+      :key="tile.id"
+      :class="[tile.value === 'X' ? selectedClass : '']"
+    >
+      <button @click="fillTile(tile.id)">
+        {{tiles[tile.id].value}}
+      </button>
+    </span>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script> //
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      player: "X",
+      tiles: [...Array(9)].map((_value, index) => {
+        return {
+          id: index,
+          disabled: false,
+          value: ""
+        }
+      }),
+      selectedClass: "selected"
+    }
+  },
+  methods: {
+    fillTile(tile) {
+      console.log(tile)
+      this.tiles[tile].value = this.player
+      console.log(this.tiles)
+      console.log(this.tiles[tile])
+    }
   }
 }
 </script>
 
 <style>
-#app {
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  background-color: black;
   text-align: center;
-  color: #2c3e50;
+  color: magenta;
   margin-top: 60px;
+}
+
+button {
+  background-color: plum;
+  width: 5em;
+  height: 5em;
+}
+
+.selected button {
+  background-color: violet;
+}
+
+#app {
+  width: min-content;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -6,3 +6,14 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+// Clear the console on reload
+if (module.hot) {
+  module.hot.accept()
+
+  module.hot.addStatusHandler((status) => {
+    if (status === "prepare") {
+      console.clear()
+    }
+  })
+}
